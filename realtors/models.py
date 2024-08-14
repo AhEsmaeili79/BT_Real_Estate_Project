2afc94b9430be416189a2f988ef5ae5f87739e6d
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-08-14 13:54:08 +0330

    add Listing model

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Realtor(models.Model):
+    name = models.CharField()
+    photo: models.CharField()
+    description: models.TextField()
+    email: models.EmailField()
+    phone: models.CharField()
+    is_mvp: models.BooleanField(0)
+    hire_date: models.DateField()

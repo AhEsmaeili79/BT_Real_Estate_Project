5949fefc40ce2e5dcedce6481a3208b939999b71
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-08-15 20:44:58 +0330

    8.5.add_login

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render,redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,'you are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request,"invalid credential")
+            return redirect('login')
     else:
         return render(request,'accounts/login.html')
 
